37d0c12857e5f1f54ab99de008d00f050b79e433
Candy <chenyanxx@126.com>
2017-02-10 16:24:41 +0800

    my java

---
+++ myjava/TestFloatType.java
@@ -0,0 +1,8 @@
+public class TestFloatType{
+	public static void main(string[] args){
+	double d = 3.14; //浮点数常量默认是double
+	float f = 6.28;
+	double d2 = 314e-2; //采用科学计数法
+	System.out.println(d2);
+	}
+}
